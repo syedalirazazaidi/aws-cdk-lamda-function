9b4abf75c84a0a1879a0af8e0b09c7e4f30bd312
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-05-25 16:50:51 +0500

    configure aws using lamda function

--- lib/todo-stack.ts
+++ lib/todo-stack.ts
@@ -1,9 +1,22 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+
+import * as apigw from "@aws-cdk/aws-apigateway";
+import * as path from "path";
 
 export class TodoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloFn = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+
+      code: lambda.Code.fromAsset("lamda"),
+      handler: "hello.handler",
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: helloFn,
+    });
   }
 }
